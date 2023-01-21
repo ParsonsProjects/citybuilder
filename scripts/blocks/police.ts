0464aec82ff9cd5e8857856b14dbf4cd561bf13d
ParsonsProjects <parsonsprojects@outlook.com>
2023-01-21 09:47:48 +0000

    1 file modified

--- scripts/blocks/police.ts
+++ scripts/blocks/police.ts
@@ -5,8 +5,12 @@ import { EBlocks } from '../../enums/blocks';
 export class Police extends Block {
   type = EBlocks.POLICE;
   coverage = 5;
+  cost = 30000;
+  rate = 400;
 
   constructor(app: Application) {
     super(app, 0x004fcf);
   }
+
+  ticker() {}
 }
